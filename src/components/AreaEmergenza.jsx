bef983b6da20a4aff13639e01b66d8bce5a606c3
gioele <gioelefriggia@hotmail.com>
2024-05-29 16:29:55 +0100

    Volontari

--- src/components/AreaEmergenza.jsx
+++ src/components/AreaEmergenza.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CorsoBlsd from "../../CorsoBlsd";
+import CorsoBlsd from "../components/volontari/CorsoBlsd";
 import CorsoPblsd from "../components/volontariPage/CorsoPblsd";
 import CorsoPtc from "../components/volontariPage/CorsoPtc";
 
